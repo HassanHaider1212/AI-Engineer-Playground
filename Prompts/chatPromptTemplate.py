942231d8889194d1c5d6ebcde5f11de34bda81cb
HassanHaider1212 <syedecommerce5912@gmail.com>
2026-02-23 03:27:34 +0500

    multiple messages placeholder fit.

---
+++ Prompts/chatPromptTemplate.py
@@ -0,0 +1,12 @@
+from langchain_core.prompts import ChatPromptTemplate
+
+chat_template = ChatPromptTemplate.from_messages([
+    ("system", "You are a helpful {domain} expert"),
+    ("human", "Explain in simple term. what is {topic}?")
+])
+
+prompt = chat_template.invoke({
+    "domain": "Cricket Expert",
+    "topic": "Dusra"
+})
+print(prompt)
