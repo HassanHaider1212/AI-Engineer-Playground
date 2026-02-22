56e604906e6977ab28b0ea9d7f920a0cb716fde1
HassanHaider1212 <syedecommerce5912@gmail.com>
2026-02-23 03:05:27 +0500

    messages concept / complete chatbot

--- Prompts/chatbot.py
+++ Prompts/chatbot.py
@@ -1,21 +1,22 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
 
 load_dotenv()
 
 model = ChatOpenAI()
 
-conversation_history = []
+conversation_history = [
+    SystemMessage(content="You are a helpful assistant that can answer questions and help with tasks."),
+]
 
 while True:
     user_input = input("You: ")
-    conversation_history.append(user_input)
+    conversation_history.append(HumanMessage(content=user_input))
     if user_input.lower() == "exit":
         break
     result = model.invoke(conversation_history)
-    conversation_history.append(result.content)
+    conversation_history.append(AIMessage(content=result.content))
     print("Bot: ", result.content)
 
 print(conversation_history)
-
-#now, issue is which message is from user and which message is from bot. maintain in a dictionary.
